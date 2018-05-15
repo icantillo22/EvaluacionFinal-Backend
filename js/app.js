4141fc4cff3bab1148828e4d6c92107660e46071
Isaac Cantillo <31421368+icantillo22@users.noreply.github.com>
2018-05-15 17:49:11 -0400

    2.0

--- js/app.js
+++ js/app.js
@@ -1,24 +1,22 @@
-$(function () {
+$(function(){
   var Propiedades = {
     formulario: $('#formulario'),
-    btnMostrar: $('#mostrarTodos'),
-    contProp: $('#Propiedades'),
+    $btnTodos: $('#mostrarTodos'),
+    contProp: $('#prop'),
 
-    Init: function () {
+    Init: function(){
       var self = this
       self.cargarSelect()
       self.cargarTodos()
-      self.formulario.submit(function (e) {
+      self.formulario.submit(function(e){
         e.preventDefault()
-        self.buscar()
+        self.buscarProp()
       })
     },
-
-    cargarSelect: function () {
+    cargarSelect: function(){
       $('select').material_select()
     },
-
-    buscar:function(e){
+    buscarProp: function(e){
       var self = this
       var ciudad = $('form').find('select[id="selectCiudad"]').val()
       var tipo = $('form').find('select[id="selectTipo"]').val()
@@ -28,5 +26,73 @@ $(function () {
       var datos = {ciudad: ciudad, tipo: tipo, from: from, to: to}
       self.ajaxData(datos)
     },
+    cargarTodos: function(){
+      var self = this
+      self.$btnTodos.on('click', (e)=>{
+        var datos = {todos: ""}
+        self.ajaxData(datos)
+      })
+    },
+    ajaxData: function(datos){
+      var self = this
+      $.ajax({
+        url: 'search.php',
+        type: 'POST',
+        data: datos
+      }).done(function(data){
+        var newData = JSON.parse(data)
+        self.renderPropiedades(newData)
+      })
+    },
+    toNumero: function(num){
+      var numero = num
+      var newNumero = Number(numero.replace('$', '').replace(',', '').replace(' ', ''))
+      return newNumero
+    },
+    renderPropiedades: function(bienes){
+      var self = this
+      var bien = bienes
+      self.contProp.html('')
+
+      bien.map((bien)=>{
+        var bienTemplate = '<div class="itemMostrado card horizontal">'+
+                                '<img src="img/home.jpg">'+
+                              '<div class="card-stacked">'+
+                                '<div class="card-content">'+
+                                  '<div>'+
+                                    '<b>Direccion: </b>:direccion:<p></p>'+
+                                  '</div>'+
+                                  '<div>'+
+                                    '<b>Ciudad: </b>:ciudad:<p></p>'+
+                                  '</div>'+
+                                  '<div>'+
+                                    '<b>Telefono: </b>:telefono:<p></p>'+
+                                  '</div>'+
+                                  '<div>'+
+                                    '<b>Código postal: </b>:codigo_postal:<p></p>'+
+                                  '</div>'+
+                                  '<div>'+
+                                    '<b>Precio: </b>:precio:<p></p>'+
+                                  '</div>'+
+                                  '<div>'+
+                                    '<b>Tipo: </b>:tipo:<p></p>'+
+                                  '</div>'+
+                                '</div>'+
+                                '<div class="card-action right-align">'+
+                                  '<a href="#">Ver más</a>'+
+                                '</div>'+
+                              '</div>'+
+                            '</div>';
+
+        var newProp = bienTemplate.replace(':direccion:', bien.Direccion)
+                                  .replace(':ciudad:', bien.Ciudad)
+                                  .replace(':telefono:', bien.Telefono)
+                                  .replace(':codigo_postal:', bien.Codigo_Postal)
+                                  .replace(':precio:', bien.Precio)
+                                  .replace(':tipo:', bien.Tipo)
+        self.contProp.append(newProp)
+      })
+    }
   }
+  Propiedades.Init()
 })
--- search.php
+++ search.php
@@ -1,31 +1,31 @@
 <?php
-$file = fopen('data-1.json');
-$data = fread($file, filesize('data-1.json'));
-$dArray = json_decode($data);
-$newData = array()
+  $file = fopen('data-1.json', "r");
+  $data = fread($file, filesize('data-1.json'));
+  $dataArray = json_decode($data);
+  $newData = array();
 
-if(isset($_POST["ciudad"]) && isset($_POST["tipo"]) && isset($_POST["from"]) && isset($_POST["to"])){
-  $ciudad = $_POST["ciudad"];
-  $tipo = $_POST["tipo"];
-  $from = $_POST["from"];
-  $to = $_POST["to"];
+  if(isset($_POST["ciudad"]) && isset($_POST["tipo"]) && isset($_POST["from"]) && isset($_POST["to"])){
+    $ciudad = $_POST["ciudad"];
+    $tipo = $_POST["tipo"];
+    $from = $_POST["from"];
+    $to = $_POST["to"];
 
-  for($i=0; $i < count($dataArray); $i++){
-    $newPrecio = str_replace('$', '', str_replace(',', '', str_replace(' ', '', $dataArray[$i]->Precio)));
-    if($newPrecio >= $from && $newPrecio <= $to && $dataArray[$i]->Ciudad == $ciudad && $dataArray[$i]->Tipo == $tipo){
-      array_push($newData, $dataArray[$i]);
-    }else if($newPrecio >= $from && $newPrecio <= $to && $dataArray[$i]->Ciudad == $ciudad && $tipo == ""){
-      array_push($newData, $dataArray[$i]);
-    }else if($newPrecio >= $from && $newPrecio <= $to && $dataArray[$i]->Tipo == $tipo && $ciudad == ""){
-      array_push($newData, $dataArray[$i]);
-    }else if($newPrecio >= $from && $newPrecio <= $to && $ciudad == "" && $tipo == ""){
-      array_push($newData, $dataArray[$i]);
+    for($i=0; $i < count($dataArray); $i++){
+      $newPrecio = str_replace('$', '', str_replace(',', '', str_replace(' ', '', $dataArray[$i]->Precio)));
+      if($newPrecio >= $from && $newPrecio <= $to && $dataArray[$i]->Ciudad == $ciudad && $dataArray[$i]->Tipo == $tipo){
+        array_push($newData, $dataArray[$i]);
+      }else if($newPrecio >= $from && $newPrecio <= $to && $dataArray[$i]->Ciudad == $ciudad && $tipo == ""){
+        array_push($newData, $dataArray[$i]);
+      }else if($newPrecio >= $from && $newPrecio <= $to && $dataArray[$i]->Tipo == $tipo && $ciudad == ""){
+        array_push($newData, $dataArray[$i]);
+      }else if($newPrecio >= $from && $newPrecio <= $to && $ciudad == "" && $tipo == ""){
+        array_push($newData, $dataArray[$i]);
+      }
     }
+    echo json_encode($newData);
+  }else if(isset($_POST["todos"])){
+    echo $data;
   }
-  echo json_encode($newData);
-}else if(isset($_POST["todos"])){
-  echo $data;
-}
 
-fclose($file);
+  fclose($file);
 ?>
